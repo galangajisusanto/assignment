51e6612c9560d5e928155b7d28a374f3f176ba59
galangaj0607 <galangajisusanto@gmail.com>
2020-08-02 17:55:36 +0700

    create network connection API

---
+++ app/src/main/java/com/galangaji/bukuwarungassignment/data/network/MyApi.kt
@@ -0,0 +1,35 @@
+package com.galangaji.bukuwarungassignment.data.network
+
+import com.galangaji.bukuwarungassignment.data.network.response.UserResponse
+import okhttp3.OkHttpClient
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+
+
+interface MyApi {
+
+
+    @GET("users")
+    suspend fun getUsers(): Response<UserResponse>
+
+
+    companion object {
+        operator fun invoke(
+            networkConnectionInterceptor: NetworkConnectionInterceptor
+        ): MyApi {
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(networkConnectionInterceptor)
+                .build()
+
+            return Retrofit.Builder().client(okHttpClient)
+                .baseUrl("https://reqres.in/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(MyApi::class.java)
+        }
+    }
+}
