e6a7f9cd516ee6fc173b0be5dfac308167771e76
galangaj0607 <galangajisusanto@gmail.com>
2020-08-02 18:05:02 +0700

    create view extension function

---
+++ app/src/main/java/com/galangaji/bukuwarungassignment/utils/ViewUtil.kt
@@ -0,0 +1,27 @@
+package com.galangaji.bukuwarungassignment.utils
+
+import android.content.Context
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun ProgressBar.show() {
+    visibility = View.VISIBLE
+}
+
+fun ProgressBar.hide() {
+    visibility = View.GONE
+}
+
+fun View.snackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_LONG).also { snackBar ->
+        snackBar.setAction("Ok") {
+            snackBar.dismiss()
+        }
+    }.show()
+}
