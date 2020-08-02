6ae5063ec062bbba668ef3d3c1a782f080e182e9
galangaj0607 <galangajisusanto@gmail.com>
2020-08-02 17:56:12 +0700

    add user repository

---
+++ app/src/main/java/com/galangaji/bukuwarungassignment/data/repositories/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.galangaji.bukuwarungassignment.data.repositories
+
+import androidx.lifecycle.LiveData
+import com.galangaji.bukuwarungassignment.data.db.entities.User
+import com.galangaji.bukuwarungassignment.data.network.response.UserResponse
+import com.galangaji.bukuwarungassignment.utils.UseCaseResult
+
+interface UserRepository {
+    suspend fun fetchUsers(): UseCaseResult<UserResponse>
+    suspend fun getUsers(): LiveData<List<User>>
+    fun saveUsers(users: List<User>)
+
+}
---
+++ app/src/main/java/com/galangaji/bukuwarungassignment/data/repositories/UserRepositoryImpl.kt
@@ -0,0 +1,66 @@
+package com.galangaji.bukuwarungassignment.data.repositories
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.galangaji.bukuwarungassignment.data.db.AppDatabase
+import com.galangaji.bukuwarungassignment.data.db.entities.User
+import com.galangaji.bukuwarungassignment.data.network.MyApi
+import com.galangaji.bukuwarungassignment.data.network.SafeApiRequest
+import com.galangaji.bukuwarungassignment.data.network.response.UserResponse
+import com.galangaji.bukuwarungassignment.utils.ApiException
+import com.galangaji.bukuwarungassignment.utils.NoInternetException
+import com.galangaji.bukuwarungassignment.utils.UseCaseResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class UserRepositoryImpl(
+    private val api: MyApi,
+    private val db: AppDatabase
+) : SafeApiRequest(), UserRepository {
+
+    private val userList = MutableLiveData<List<User>>()
+
+    init {
+        userList.observeForever {
+            saveUsers(it)
+        }
+    }
+
+
+    override suspend fun fetchUsers(): UseCaseResult<UserResponse> {
+        return try {
+            var result = apiRequest { api.getUsers() }
+            UseCaseResult.Success(result)
+        } catch (ex: ApiException) {
+            UseCaseResult.Error(ex)
+        } catch (ex: NoInternetException) {
+            UseCaseResult.Error(ex)
+        } catch (ex: Exception) {
+            UseCaseResult.Error(ex)
+        }
+    }
+
+    override suspend fun getUsers(): LiveData<List<User>> {
+        return withContext(Dispatchers.IO) {
+
+            when (val response = fetchUsers()) {
+                is UseCaseResult.Success -> {
+                    userList.postValue(response.data.data)
+                    Log.d("data_user", response.data.data.toString())
+                }
+            }
+
+            db.getUserDao().getAllUsers()
+        }
+    }
+
+    override fun saveUsers(users: List<User>) {
+        CoroutineScope(Dispatchers.IO).launch {
+            db.getUserDao().saveAllUsers(users)
+        }
+
+    }
+}
